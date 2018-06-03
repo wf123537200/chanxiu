6226a66a5477e55985fee50e27d4d3dc9323a9e3
wufeng <123537200@qq.com>
2018-06-03 21:46:06 +0800

    first commit

---
+++ src/components/ajax.js
@@ -0,0 +1,55 @@
+import wepy from 'wepy'
+
+export const ajax = function ({ins, url, params = {}, method = 'GET', success, fail}) {
+  if(!ins) console.error('没有传入实体！');
+
+  const {ajaxPerfix, resPerfix} = ins.globalData || ins.$parent.globalData;
+  const token = url !== '/weixin/token' ? ins.accessToken || ins.$parent.accessToken : ''
+
+  wx.getNetworkType({
+    success: (res) => {
+      const networkType = res.networkType
+      if(networkType === 'none' || networkType === 'unkonwn' && method === 'GET') {
+        // 拿本地存储
+        const d = wx.getStorageSync(ajaxPerfix + url);
+        if(d) {
+          success && success(d)
+        } else {
+          fail ? fail() : wx.showToast({
+            title: data.data.msg || '调用接口失败',
+            icon: 'error',
+            duration: 2000,
+            mask:true
+          })
+
+          throw new Error(data.data.msg + ':' + url)
+        }
+      } else {
+        wepy.request({url: ajaxPerfix + url, data: Object.assign(params, {'access_token': token}), method, header: {'content-type': 'application/x-www-form-urlencoded'}}).then(data => {
+          if(data.data.code === 0) {
+            // 存储
+            if(method === 'GET') wx.setStorage({
+              key: ajaxPerfix + url,
+              data: data.data.data,
+              success: function(res){
+                console.log('本地存储 ' + ajaxPerfix + url + '成功')
+              }
+            })
+
+            success && success(data.data.data)
+          } else {
+            fail && fail()
+            wx.showToast({
+              title: data.data.msg || '调用接口失败',
+              icon: 'error',
+              duration: 2000,
+              mask:true
+            })
+
+            throw new Error(data.data.msg + ':' + url)
+          }
+        })
+      }
+    }
+  })
+}
