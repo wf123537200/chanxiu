ba15c323862091a1107326b0afd900e1d7c12828
Huangshuai <stubend@163.com>
2018-10-03 01:56:31 +0800

    模板消息收集

--- src/modules/Common.js
+++ src/modules/Common.js
@@ -6,10 +6,6 @@ import ajax2promise from "../components/ajax2promise";
 import getGlobalData from "../components/getGlobalData";
 
 export async function collectFormId (params = {}) {
-    let _global = getGlobalData(this);
-    let {ins, data} = _global;
-    let formIds = data.formIds;
-
     await ajax2promise({
         ins: this,
         method: "POST",
